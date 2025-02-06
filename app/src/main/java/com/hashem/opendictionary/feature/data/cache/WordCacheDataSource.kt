013fb988bf051fc485d0acebe23c2f8807e3129f
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-06 20:12:19 +0200

    adding cache datasource

---
+++ app/src/main/java/com/hashem/opendictionary/feature/data/cache/WordCacheDataSource.kt
@@ -0,0 +1,20 @@
+package com.hashem.opendictionary.feature.data.cache
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.hashem.opendictionary.feature.data.cache.models.WordCache
+
+@Dao
+interface WordCacheDataSource {
+
+    @Query("SELECT * FROM WordCache")
+    suspend fun getWords(): List<WordCache>
+
+    @Query("SELECT * FROM WordCache WHERE word = :word")
+    suspend fun getWord(word: String): List<WordCache>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWord(word: WordCache)
+}
