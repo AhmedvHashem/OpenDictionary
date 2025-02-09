18256b39613c9580a16b80da064831ab211b8ae7
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-09 16:55:05 +0200

    add unit tests for GetWordUseCase, using WordRepository

---
+++ app/src/test/java/com/hashem/opendictionary/usecases/GetWordUseCaseTest.kt
@@ -0,0 +1,96 @@
+package com.hashem.opendictionary.usecases
+
+import com.hashem.opendictionary.feature.domain.GetWordUseCase
+import com.hashem.opendictionary.feature.domain.models.Definition
+import com.hashem.opendictionary.feature.domain.models.Meaning
+import com.hashem.opendictionary.feature.domain.models.Phonetic
+import com.hashem.opendictionary.feature.domain.models.Word
+import com.hashem.opendictionary.feature.domain.repository.WordError
+import com.hashem.opendictionary.feature.domain.repository.WordRepository
+import com.hashem.opendictionary.feature.domain.repository.WordResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
+
+class GetWordUseCaseTest {
+
+    private lateinit var repository: WordRepository
+    private lateinit var getWordUseCase: GetWordUseCase
+
+    @Before
+    fun setUp() {
+        repository = mock()
+        getWordUseCase = GetWordUseCase(repository, Dispatchers.Unconfined)
+    }
+
+    @Test
+    fun `invoke should return word from repository`() = runTest {
+        val word = "example"
+        val expectedWord = Word(
+            word, Phonetic("qwe", "audio"), mapOf(
+                "noun" to Meaning(
+                    listOf(
+                        Definition("meaning", "example")
+                    ), setOf("synonym"), setOf("antonym")
+                )
+            )
+        )
+        whenever(repository.getWord(word)).thenReturn(flowOf(WordResult.Success(listOf(expectedWord))))
+
+        val resultFlow = getWordUseCase(word)
+
+        resultFlow.collect { result ->
+            assertEquals(WordResult.Success(listOf(expectedWord)), result)
+        }
+    }
+
+    @Test
+    fun `invoke should return NotFoundError if word is not found`() = runTest {
+        val word = "nonexistent"
+        whenever(repository.getWord(word)).thenReturn(flowOf(WordResult.Fail(WordError.NotFoundError)))
+
+        val resultFlow = getWordUseCase(word)
+
+        resultFlow.collect { result ->
+            assertEquals(WordResult.Fail(WordError.NotFoundError), result)
+        }
+    }
+
+    @Test
+    fun `invoke should return ApiError on server error`() = runTest {
+        val word = "example"
+        whenever(repository.getWord(word)).thenReturn(flowOf(WordResult.Fail(WordError.ApiError)))
+
+        val resultFlow = getWordUseCase(word)
+
+        resultFlow.collect { result ->
+            assertEquals(WordResult.Fail(WordError.ApiError), result)
+        }
+    }
+
+    @Test
+    fun `invoke should return UnknownError for custom error`() = runTest {
+        val word = "example"
+        val exceptionMessage = "Unknown Exception"
+        whenever(repository.getWord(word)).thenReturn(
+            flowOf(
+                WordResult.Fail(
+                    WordError.UnknownError(
+                        exceptionMessage
+                    )
+                )
+            )
+        )
+
+        val resultFlow = getWordUseCase(word)
+
+        resultFlow.collect { result ->
+            assertEquals(WordResult.Fail(WordError.UnknownError(exceptionMessage)), result)
+        }
+    }
+}
